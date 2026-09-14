83de9a7f5d531dfca44a0d82d09129734dd3d14c
realfaizandev <s.faizanalib@gmail.com>
2026-09-14 16:21:58 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* themeBtn */}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+      </div>
+    </div>
+  );
+}
